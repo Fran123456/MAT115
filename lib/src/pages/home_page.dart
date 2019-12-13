b02f003c98219fc37ab0d3c583b97bf090a77bf1
Fran Navas <navasfran98@gmail.com>
2019-12-13 15:06:12 -0600

    nuevas actualizaciones

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:icons_helper/icons_helper.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -18,14 +19,36 @@ class HomePage extends StatelessWidget {
  //Retorna la lista que usaremos en el body
   Widget _lista() {
 
-    
-     return ListView(
-       children: _listaItems(),
+     return FutureBuilder(
+       future: menuProvider.cargarData(),
+       initialData: [],
+       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+         return ListView(
+           children: _listaItems(snapshot.data),
+         );
+       },
      );
   }
 
+
   //retorna listas
-  List<Widget>  _listaItems(){
-      return[];
+  List<Widget>  _listaItems(List<dynamic> data){
+      
+     final List<Widget> opciones = [];
+     for (var op in data) {
+
+       final widgetTemp =  ListTile(
+         title: Text(op['texto']),
+         leading: Icon(getIconGuessFavorMaterial(name:op['icon']) ,color: Colors.blue),
+         trailing: Icon(Icons.keyboard_arrow_right,color: Colors.blue),
+         onTap: (){
+          Navigator.pushNamed(context, op['ruta']);
+         },
+       );
+       opciones.add(widgetTemp);
+       opciones.add(Divider());
+
+     }
+     return opciones;
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -5,15 +5,12 @@ class _MenuProvider{
 
   List<dynamic> opciones = [];
   
-  _MenuProvider(){
-    cargarData();
-  }
+  Future<List<dynamic>>  cargarData() async{
+   final resp = await rootBundle.loadString('data/menu_opts.json');
+   Map dataMap = json.decode(resp);
+   opciones = dataMap['rutas'];
 
-  cargarData(){
-    rootBundle.loadString('data/menu_opts.json').then((data){
-      Map dataMap = json.decode(data);
-    });
-
+   return opciones;
   }
 }
 
